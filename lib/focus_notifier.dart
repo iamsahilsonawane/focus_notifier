683ad9e9bb521b13204de734df3065ce15c306dd
Sahil Sonawane <iamsahilsonawane@gmail.com>
2022-08-11 18:26:16 +0530

    fix: named constructor for custom focus nodes

--- lib/focus_notifier.dart
+++ lib/focus_notifier.dart
@@ -9,15 +9,17 @@ typedef FocusNotifierBuilder = Widget Function(
     BuildContext context, FocusNode focusNode);
 
 ///Wrap this widget over a focus widget.
-///
 ///Assign the focus node to the child focus widget
 ///
-///Use [key] to add custom key for referenceing it in the listener.
+///Use [key] to add custom key for referencing it in the listener.
+///
+///If [FocusNotifier.customFocusNode] is used the custom focus node provided will not be disposed.
 class FocusNotifier extends StatefulWidget {
-  const FocusNotifier({Key? key, required this.builder}) : super(key: key);
+  const FocusNotifier({Key? key required this.builder}) : super(key: key);
+  const FocusNotifier.customFocusNode({Key? key required this.builder, required this.focusNode}) : super(key: key);
 
-  final FocusNode? customFocusNode;
   final FocusNotifierBuilder builder;
+  final FocusNode? focusNode;
 
   @override
   State<FocusNotifier> createState() => _FocusNotifierState();
@@ -28,8 +30,8 @@ class _FocusNotifierState extends State<FocusNotifier> {
 
   @override
   initState() {
-    if (widget.customFocusNode != null) {
-      node = widget.customFocusNode;
+    if (widget.focusNode != null) {
+      node = widget.focusNode;
     }
     node.addListener(() {
       if (node.hasFocus) {
@@ -42,7 +44,7 @@ class _FocusNotifierState extends State<FocusNotifier> {
 
   @override
   dispose() {
-    if (widget.customFocusNode == null) node.dispose();
+    if (widget.focusNode == null) node.dispose();
     super.dispose();
   }
 
